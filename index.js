8b6975dc35845a530638ec976165deeefc16bb35
SidnyZH\Sidny <syzapataho@gmail.com>
2023-06-05 19:58:54 -0500

    -Clases

---
+++ index.js
@@ -0,0 +1,14 @@
+class Cliente 
+{
+    nombre; 
+    dni;
+    numeroDeCuenta;
+    saldoDeLaCuenta;
+}
+const cliente1 = new Cliente();
+
+cliente1.nombre = "sidny"; 
+cliente1.numeroDeCuenta = " 1235423453"; 
+cliente1.saldoDeLaCuenta = "10000000";
+
+console.log(cliente1)
