80a65acdc7c8713d8e2730586e79c79bbd0f5c99
SidnyZH\Sidny <syzapataho@gmail.com>
2023-06-07 18:04:43 -0500

    Fixing the access to private properties

--- index.js
+++ index.js
@@ -10,33 +10,44 @@ class Cliente
 class CuentaCorriente
 {
     numero; 
-    saldo; 
+    // este # hace la propieda privada, esto quere decir que solo se modifica dentro de la clase, a través de sus métodos
+    #saldo; 
     agencia; 
 
+    constructor() {
+        // toda cuenta que se cree va a tener su  #saldo en 0
+        this. #saldo = 0; 
+        this.numero = ""; 
+        this.agencia = "";
+    }
+
     depositoEnCuenta(valor){
-        this.saldo = this.saldo + valor; 
+        return this. #saldo += valor; 
     }
 
     retirarEnCuenta(valor){
-        if (valor > this.saldo) {
-            console.log("El monto a retirar supera el saldo diponible")
+        if (valor > this. #saldo) {
+            return "El monto a retirar supera el  #saldo diponible";
         }
         else{
-            this.saldo = this.saldo -= valor; 
+            this.#saldo = this. #saldo -= valor; 
+            return this.#saldo;
         }
+    }
+    verSAldo(){
+        return this.#saldo;
     }
 }
 
 
 const cliente1 = new Cliente();
 const CuentaCorriente1 = new CuentaCorriente();
-
+let saldo = CuentaCorriente1.verSAldo();
 cliente1.nombre = "sidny"; 
 CuentaCorriente1.numero = "125646654";
-CuentaCorriente1.saldo = "10000000";
 
 CuentaCorriente1.depositoEnCuenta(10000)
-console.log(CuentaCorriente1.saldo);
+console.log(saldo);
 
 CuentaCorriente1.retirarEnCuenta(2000);
-console.log(CuentaCorriente1.saldo)
+console.log(saldo)
